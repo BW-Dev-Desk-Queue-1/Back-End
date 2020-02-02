1b52e6172065b80719d97e10b0f0978593e7462f
Young Won2 <youngw417@MacBook.local>
2020-02-02 12:11:19 -0800

    user-router and userModel updated for get request

--- users/user-router.js
+++ users/user-router.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
 const User = require('./userModel');
 
+// get all tickets for a user
 router.get('/:userId/tickets', (req, res, next) => {
   const { userId } = req.params;
   console.log('req.params', req.params);
-  User.findByUserId(userId)
+  User.findAllTicketsByUserId(userId)
     .then(user => res.status(200).json(user))
     .catch(err => next(err));
 });
 module.exports = router;
+
+// get a single ticket information for a user
+router.get('/:userId/tickets/:ticketId', (req, res, next) => {
+  const { userId, ticketId } = req.params;
+  User.findTicketByUserId(userId, ticketId)
+    .then(item => res.status(200).json(item))
+    .catch(err => next(err));
+});
+
+// create a ticket for a user
+
+// upate a ticket for a user
+
+// delete a ticket for a user
--- users/userModel.js
+++ users/userModel.js
@@ -1,9 +1,68 @@
 const UserDb = require('../config/dbConfig');
 
 module.exports = {
-  findByUserId
+  findAllTicketsByUserId,
+  findTicketByUserId
 };
 
-function findByUserId(id) {
-  return UserDb('users').where({ id });
+function toResolved(bool) {
+  if (bool) return true;
+  else return false;
 }
+// find all tickets
+
+async function findUserTickets(id) {
+  const myTickets = await UserDb('tickets').where('user_id', id);
+
+  myTickets &&
+    myTickets.map(el => {
+      el.resolved = toResolved(el.resolved);
+      return el;
+    });
+
+  return myTickets;
+}
+
+// find a user by Id
+function findByUserId(id) {
+  return UserDb('users')
+    .where({ id })
+    .first();
+}
+
+// get all tickets for a user
+function findAllTicketsByUserId(id) {
+  return findByUserId(id).then(async user => {
+    user.password = '*********';
+    const myTickets = await findUserTickets(id);
+    console.log('mytickets', myTickets);
+    const toResponse = {
+      ...user,
+      tickets: myTickets
+    };
+    return toResponse;
+  });
+}
+
+// get a single ticket information for a user
+
+function findTicketByUserId(userId, ticketId) {
+  return findByUserId(userId).then(async user => {
+    const myTicket = await UserDb('tickets')
+      .where({ id: ticketId, user_id: userId })
+      .first();
+
+    user.password = '*******';
+    const UserTicket = {
+      ...user,
+      ticket: myTicket || 'no Ticket Found'
+    };
+    return UserTicket;
+  });
+}
+
+// create a ticket for a user
+
+// upate a ticket for a user
+
+// delete a ticket for a user
