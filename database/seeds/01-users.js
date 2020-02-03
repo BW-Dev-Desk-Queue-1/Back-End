bbd357ac50decdaa074083eb24f32b68c3bf52ab
David Tauraso <david318@me.com>
2020-02-03 12:03:10 -0800

    Updated user seed code

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,11 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, username: 'young123', password: '123456', accessType: 'student'},
         {id: 2, username: 'james123', password: '123456', accessType: 'student'},
         {id: 3, username: 'sunny123', password: '123456', accessType: 'student'}
       ]);
-    
+    });
 };
