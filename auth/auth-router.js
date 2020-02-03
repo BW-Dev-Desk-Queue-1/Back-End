49ad2d996443c360970d9905092ad981b2b30eb6
Young Won2 <youngw417@MacBook.local>
2020-02-03 14:46:53 -0800

    revised auth-route with accessType

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,63 +1,58 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { jwtSecret } = require('../config/secrets.js')
-const Users = require('../users/userModel.js')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/secrets.js');
+const Users = require('../users/userModel.js');
 
 router.post('/register', (req, res, next) => {
-    let user = req.body
+  let user = req.body;
 
-    const hash = bcrypt.hashSync(user.password, 5); // 2 ^ n
+  const hash = bcrypt.hashSync(user.password, 5); // 2 ^ n
 
-    user.password = hash;
-    console.log('here')
-    Users.addUser(user)
-        .then(saved => {
-            res.status(201).json(user);
-        })
-        .catch(error => {
-            next(error);
-        })
-    
-})
+  user.password = hash;
+  console.log('here');
+  Users.addUser(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 router.post('/login', (req, res, next) => {
-    let { username, password } = req.body
+  let { username, password } = req.body;
 
-    Users.findByUserName( username )
-        .first()
-        .then(user => {
+  Users.findByUserName(username)
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
 
-            if(user && bcrypt.compareSync(password, user.password)) {
-                
-                const token = signToken(user)
-
-                res.status(200).json({ token })
-
-            } else {
-                res.status(401).json({ message: 'Invalid Credentials'})
-            }
-        })
-        .catch(error => {
-            next(error);
-        })
-})
+        res.status(200).json({ token, accessType: user.accessType });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 //hoisted to top of scope
 function signToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username,
+    userAccessType: user.accessType
+  };
 
-    const payload = {
-        userId: user.id,
-        username: user.username,
-        userAccessType: user.accessType
-    }
+  const options = {
+    expiresIn: '1d'
+  };
 
-    const options = {
-        expiresIn: '1d'
-    }
-
-    return jwt.sign(payload, jwtSecret, options)
+  return jwt.sign(payload, jwtSecret, options);
 }
 
-module.exports = router
+module.exports = router;
