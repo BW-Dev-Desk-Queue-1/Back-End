8001dc090261e91e3d4f326f450c5e47c9e1f7c3
David Tauraso <david318@me.com>
2020-02-03 16:02:42 -0800

    Added a text field called tried to the ticket

--- database/seeds/03-tickets.js
+++ database/seeds/03-tickets.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('tickets').insert([
-        {id: 1, title: 'registration question', description: 'When is the next registration deadline?', ticketCategory: 'frontOffice', user_id: 3, helper_id: 2},
-        {id: 2, title: 'progress report', description: 'my progress report is not updating since Jan. 2020', ticketCategory: 'technical', user_id: 2, helper_id: 1},
-        {id: 3, title: 'Slack', description: 'My userid is not working in Slack', ticketCategory: 'technical', user_id: 3, helper_id: 3}
+        {id: 1, title: 'registration question', description: 'When is the next registration deadline?', ticketCategory: 'frontOffice', tried: '', user_id: 3, helper_id: 2},
+        {id: 2, title: 'progress report', description: 'my progress report is not updating since Jan. 2020', ticketCategory: 'technical', tried: 'I restarted the computer', user_id: 2, helper_id: 1},
+        {id: 3, title: 'Slack', description: 'My userid is not working in Slack', ticketCategory: 'technical', tried: 'I did make an account but...', user_id: 3, helper_id: 3}
       ]);
     });
 };
