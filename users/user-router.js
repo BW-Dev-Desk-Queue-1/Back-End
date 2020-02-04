699d23612a4b16e8018a0744e104952fd7ff7fe3
Young Won2 <youngw417@MacBook.local>
2020-02-03 17:23:26 -0800

    Young-

--- users/user-router.js
+++ users/user-router.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 const User = require('./userModel');
 const authenticate = require('../auth/authenticate-middleware.js');
 
-
-
-
 router.get('/', (req, res) => {
-
   User.find()
     .then(users => {
       User.getUsersTickets(users).then(tickets => {
@@ -24,6 +20,7 @@ router.get('/', (req, res) => {
 // get all tickets for a user
 router.get('/:userId/tickets', authenticate, (req, res, next) => {
   const { userId } = req.params;
+
   console.log('req.params', req.params);
   User.findAllTicketsByUserId(userId)
     .then(user => res.status(200).json(user))
