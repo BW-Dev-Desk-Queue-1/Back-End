2b55f4f38582da14bab247378dd1285c00f429cb
Young Won2 <youngw417@MacBook.local>
2020-02-04 13:40:33 -0800

    addhelper changed

--- helpers/helperModel.js
+++ helpers/helperModel.js
@@ -1,44 +1,39 @@
 const HelperDb = require('../config/dbConfig');
 // student ID
-  // access own tickets
-  // create new tickets
+// access own tickets
+// create new tickets
 // helper ID
-  // colective ticket pool
-  // see own tickets
-  // reactions
-    // adding comments
+// colective ticket pool
+// see own tickets
+// reactions
+// adding comments
 module.exports = {
-    addHelper,
-    findByHelperName,
-    find,
-    findByHelperId
-    
-    
-    };
+  addHelper,
+  findByHelperName,
+  find,
+  findByHelperId
+};
 // my code
 function find() {
-return HelperDb('helpers').select('id', 'username', 'password', 'accessType');
+  return HelperDb('helpers').select('id', 'username', 'password', 'accessType');
 }
 function addHelper(helper) {
-return HelperDb('helpers')
-        .insert(helper)
-        .then(([id]) => {
-            console.log(id)
-        return findByHelperId(id)
-        })
+  return HelperDb('helpers')
+    .insert(helper, 'id')
+    .then(([id]) => {
+      console.log(id);
+      return findByHelperId(id);
+    });
 }
 
 function findByHelperName(userName) {
-return HelperDb('helpers')
-        .where('username', userName)
+  return HelperDb('helpers').where('username', userName);
 }
-
 
 // Won's code
 // find a user by Id
 function findByHelperId(id) {
-    return HelperDb('helpers')
-      .where( 'id', id )
-      .first();
-  }
-  
+  return HelperDb('helpers')
+    .where('id', id)
+    .first();
+}
