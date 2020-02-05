5bf5c44a139fa99803f2fe219831932c9512848c
Young Won <54904636+youngw417@users.noreply.github.com>
2020-02-05 15:43:40 -0800

    Update user-router.js

--- users/user-router.js
+++ users/user-router.js
@@ -4,7 +4,7 @@ const authenticate = require('../auth/authenticate-middleware.js');
 // middleware for admin and helpers only
 const onlyFor = require('../auth/onlyFor');
 
-router.get('/', (req, res) => {
+router.get('/', authenticate, onlyFor, (req, res) => {
   User.find()
     .then(users => {
       User.getUsersTickets(users).then(tickets => {
